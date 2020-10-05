f57f3443e6df4a968c8899d62e414671e71fea01
Mateusz Nowak <mateusz.nowak@vonage.com>
2020-10-05 21:09:44 +0200

    Fix styles

--- src/quiz-hall-of-fame/presentation/QuizHallOfFameView.js
+++ src/quiz-hall-of-fame/presentation/QuizHallOfFameView.js
@@ -1,3 +1,29 @@
+const emptyBoardTemplateHtml = `
+<div>
+    <h2>Leaderboard is empty...</h2>
+</div>
+`
+const bestScoresHtml = (bestScores) => bestScores.map(({playerName, score}, place) =>
+    `
+          <div class="swquiz-hall-of-fame-player-row">
+            <p class="swquiz-hall-of-fame-player-place">${place + 1}</p>
+            <p class="swquiz-hall-of-fame-player-name">${playerName}</p>
+            <p class="swquiz-hall-of-fame-player-score">${score}</p>
+          </div>
+        `
+).reduce((x, y) => x + y)
+
+const filledBoardTemplateHtml = (bestScoresHtml) => `
+<div class="swquiz-hall-of-fame-header">
+    <p>Place</p>
+    <p>Player</p>
+    <p>Scores</p>
+</div>
+<div class="swquiz-hall-of-fame-table">
+${bestScoresHtml}
+</div>
+`
+
 export const QuizHallOfFameView = ({renderOn, presenterSupplier}) => {
   const modeContentElement = document.querySelector(renderOn)
   modeContentElement.innerHTML = ''
@@ -6,35 +32,7 @@ export const QuizHallOfFameView = ({renderOn, presenterSupplier}) => {
       presenter.loadBestScores();
     },
     showBestScores({bestScores}) {
-      let innerHtml = ''
-      if (bestScores.length === 0) {
-        innerHtml += `
-          <div>
-            <h2>Leaderboard is empty...</h2>
-          </div>
-        `
-        modeContentElement.innerHTML = innerHtml
-        return
-      }
-      innerHtml += `
-         <div class="swquiz-hall-of-fame-header">
-            <p>Place</p>
-            <p>Player</p>
-            <p>Scores</p>
-          </div>
-        `
-      innerHtml += `<div class="swquiz-hall-of-fame-table">`
-      bestScores.forEach(({playerName, score}, place) => {
-        innerHtml += `
-          <div class="swquiz-hall-of-fame-player-row">
-            <p class="swquiz-hall-of-fame-player-place">${place + 1}</p>
-            <p class="swquiz-hall-of-fame-player-name">${playerName}</p>
-            <p class="swquiz-hall-of-fame-player-score">${score}</p>
-          </div>
-        `
-      })
-      innerHtml += '</div>'
-      modeContentElement.innerHTML = innerHtml
+      modeContentElement.innerHTML = bestScores.length === 0 ? emptyBoardTemplateHtml : filledBoardTemplateHtml(bestScoresHtml(bestScores))
     }
   };
   const presenter = presenterSupplier(view);
