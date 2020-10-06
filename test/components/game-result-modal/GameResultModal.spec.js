ed67e276a89d1fe91766a47873f6e669ddcd4270
Mateusz Nowak <mateusz.nowak@vonage.com>
2020-10-06 22:04:17 +0200

    Setups tests

--- test/components/game-result-modal/GameResultModal.spec.js
+++ test/components/game-result-modal/GameResultModal.spec.js
@@ -1,8 +1,25 @@
+import {GameResultModal} from "../../../src/components/game-result-modal/GameResultModal";
+import {aModalData} from "./fixtures";
+
 describe("Component | Game Result Modal", () => {
 
-  it('use jsdom in this test file', () => {
-    const element = document.createElement('div');
-    expect(element).not.toBeNull();
+  const modal = GameResultModal()
+  modal.show({data: aModalData})
+
+  describe('when submit score save', () => {
+
+    const scoreSavedHook = jest.fn();
+    modal.onScoreSave(scoreSavedHook)
+
+    it('then score saved hooks should be notified', () => {
+      const playerName = "Sample Player Name"
+      modal.submitScoreSave({playerName})
+
+      expect(scoreSavedHook).toBeCalledTimes(1);
+      expect(scoreSavedHook).toBeCalledWith({playerName});
+    })
+
+
   });
 
 })
